5421a0ea460df9a7dbb6c22fe98f60a555329bea
mehdi snaiki <mehdisnaikidono2@gmail.com>
2021-05-27 16:02:40 +0100

    restructure

--- com.mehdi.rstapi/src/main/java/com/example/demo/Services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import com.example.demo.shared.UserDto;
-
-public interface UserService extends UserDetailsService {
-UserDto createUser(UserDto userDto);
-}
--- com.mehdi.rstapi/src/main/java/com/example/demo/security/SecurityConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.security;
-
-public class SecurityConstant {
-public static final long EXPIRATION_TIME=864000000;
-public static final String TOKEN_PREFIX="bearer ";
-public static final String HEADER_STRING="Authorization";
-public static final String SIGN_UP_URL="/users";
-public static final String TOKEN_SECRET="EE7f:t(oA6(S|O,,njpW(-aPN4yRy2%&6-,Az%lvOP|$y072xp";
-}
